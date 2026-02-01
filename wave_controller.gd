b18db6281ed9592affaa91e4cfb22b00604521cc
Antonio Dell'Annunziata <contact@antonio-da.dev>
2026-02-01 19:23:35 +0100

    Fix path selection for attackers

--- wave_controller.gd
+++ wave_controller.gd
@@ -17,10 +17,12 @@ func _on_timeout():
 	var attacker_controller: AttackerController = get_tree().get_first_node_in_group("AttackerController")
 	_spawn_attacker()
 
+var t = 0
 var next_spawn_point := 0
 func _spawn_attacker():
 	var attacker: Attacker = attacker_scene.instantiate()
 	var spawn_point = attacker_spawn_points[next_spawn_point]
 	attacker.name = "Attacker%s_0" % next_spawn_point
 	spawn_point.add_child(attacker, true)
-	next_spawn_point = pingpong(next_spawn_point + 1, attacker_spawn_points.size() - 1)
+	next_spawn_point = pingpong(t, attacker_spawn_points.size() - 1)
+	t += 1
